6af3e2e45026ac30d12f357c05753046cbf4dbc0
Antoine <136310380+antoine-codeur@users.noreply.github.com>
2025-02-07 16:43:57 +0100

    add AdviceComponent tests to verify advice display and navigation

--- src/app/spy-expert/advice/advice.component.spec.ts
+++ src/app/spy-expert/advice/advice.component.spec.ts
@@ -3,6 +3,7 @@ import { Advice } from '../core/models/advice';
 import { AdviceComponent } from './advice.component';
 import { DebugElement } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { Router } from '@angular/router';
 import { By } from '@angular/platform-browser';
 
 describe('AdviceComponent', () => {
@@ -10,6 +11,7 @@ describe('AdviceComponent', () => {
   let component: AdviceComponent;
   let fixture: ComponentFixture<AdviceComponent>;
   let debugEl: DebugElement;
+  let router: Router;
   let expectedAdvice: Advice = { slip: { id: 2, advice: 'fake advice'}};
 
   beforeEach(async () => {
@@ -21,14 +23,22 @@ describe('AdviceComponent', () => {
     fixture = TestBed.createComponent(AdviceComponent);
     component = fixture.componentInstance;
     debugEl = fixture.debugElement;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
-
-    fixture.componentRef.setInput('advice', expectedAdvice)
+    fixture.componentRef.setInput('advice', expectedAdvice);
     await fixture.whenStable();
   });
 
-  it('should display the advice', () => {});
+  it('should display the advice', () => {
+    const paragraph = debugEl.query(By.css('.quote'));
+    fixture.detectChanges();
+    const paragraphText = paragraph.nativeElement.textContent.trim();
+    expect(paragraphText).toBe(expectedAdvice.slip.advice);
+  });
 
-  // Use spy
-  it('should redirect to the hate page', () => {});
+  it('should redirect to the hate page', () => {
+    const routerSpy = spyOn(router, 'navigate');
+    component.hateAdvice();
+    expect(routerSpy).toHaveBeenCalledWith(['/hate', expectedAdvice.slip.id]);
+  });
 });
